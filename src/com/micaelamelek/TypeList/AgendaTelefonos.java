077b88d12b9e6dd71fc2cc18b424bf9910af3515
meme <micaelamelek@gmail.com>
2015-05-19 00:00:27 -0300

    add searches

--- src/com/micaelamelek/TypeList/AgendaTelefonos.java
+++ src/com/micaelamelek/TypeList/AgendaTelefonos.java
@@ -2,41 +2,64 @@ package com.micaelamelek.TypeList;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class AgendaTelefonos {
-	
-private Map<String,Integer> agenda;
 
-public AgendaTelefonos(){
-	this.agenda = new TreeMap<String, Integer>();
-	
-}
+	private Map<String, Integer> agenda;
 
-public Map<String,Integer> getAgenda(){
-	return this.agenda;
-}
+	public AgendaTelefonos() {
+		this.agenda = new TreeMap<String, Integer>();
 
-public void AgregarC (String nombre, int numero){
-	this.agenda.put(nombre, numero);
-	System.out.println("Se agrego el contacto: "+nombre);
-}
-
-public void EliminarC (String nombre){
-	this.agenda.remove(nombre);
-}
-
-public void MostrarAgenda(){
-	for (Map.Entry<String,Integer> entry : agenda.entrySet()) {
-	     System.out.println("Nombre: " + entry.getKey() + ". Telefono: " + entry.getValue());
-}
-}
-
-public void buscarTelefono(String nombre){
-	if(agenda.containsKey(nombre)){
-		System.out.println("El telefono de: "+nombre+" , es: "+agenda.get(nombre));
-		
 	}
-}
+
+	public Map<String, Integer> getAgenda() {
+		return this.agenda;
+	}
+
+	public void AgregarC(String nombre, int numero) {
+		this.agenda.put(nombre, numero);
+		System.out.println("Se agrego el contacto: " + nombre);
+	}
+
+	public void EliminarC(String nombre) {
+		this.agenda.remove(nombre);
+	}
+
+	public void MostrarAgenda() {
+		for (Map.Entry<String, Integer> entry : agenda.entrySet()) {
+			System.out.println("Nombre: " + entry.getKey() + ". Telefono: "
+					+ entry.getValue());
+		}
+	}
+
+	public void buscarNombre(Integer telefono) {
+		for (Entry<String, Integer> entry : agenda.entrySet()) {
+			if (telefono.equals(entry.getValue())) {
+				System.out.println("El telefono " + telefono + " es: "
+						+ entry.getKey());
+
+			} else {
+				System.out.println("El telefono no existe");
+			}
+
+		}
+	}
+
+	public void buscarTelefono(String nombre) {
+
+		for (Entry<String, Integer> entry : agenda.entrySet()) {
+			if (nombre.equals(entry.getKey())) {
+				System.out.println("El contacto " + nombre
+						+ " le corresponde el telefono: " + entry.getValue());
+
+			} else {
+				System.out.println("El contacto no existe");
+			}
+
+		}
+
+	}
 
 }
